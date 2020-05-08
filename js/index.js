e6b8445c9b67b38c827b77d64cadc2c4a78d3e0c
0zyxel0 <calvin.ongkingco@gmail.com>
2020-05-08 10:46:50 +0200

    updated js files of index navbar

--- js/index.js
+++ js/index.js
@@ -1,36 +1,4 @@
 
-function gotoParentElement(){
-    var currentDropDownButton = event.target;
-    var currentDropDownMenu =
-        currentDropDownButton.parentNode.querySelector('li');
-    currentDropDownMenu.focus();
-
-}
-
-
-$(document).keydown(function(e) {
-if (e.keyCode == 27 ) { // escape key maps to keycode `27`
-    event.stopPropagation();
-    event.preventDefault();
-
-    try{
-        var currentDropDownButton = event.target;
-        var currentDropDownMenu =
-            currentDropDownButton.parentNode.querySelector('.dropdown-menu');
-        var isOpen = currentDropDownMenu.classList.contains('show');
-        var dropDownMenus =
-            document.querySelectorAll('#nav-bar-content .dropdown .dropdown-menu');
-        for (var j = 0; j < dropDownMenus.length; j++) {
-            dropDownMenus[j].classList.remove('show');
-        }
-        gotoParentElement();
-        return false;
-    }catch (e) {
-        return e;
-    }
-}
-});
-
 $(document).keydown(function(e) {
     if (e.keyCode == 32) {
         event.stopPropagation();
@@ -57,3 +25,25 @@ $(document).keydown(function(e) {
         }
     }
 });
+
+    $(document).keyup(function(e){
+        if(e.keyCode == 27){
+            if($('a.dropdown-item').is(":focus") == true){
+                event.stopPropagation();
+                event.preventDefault();
+                    if($("ul.dropdown-menu").hasClass('show')){
+                        $("ul.dropdown-menu").removeClass('show');
+                        $("ul.dropdown-menu").parents('.nav-item').find('.dropdown').addClass('asdf');
+
+                    }
+            }
+        }
+    });
+
+    $(document).keyup(function(e){
+        if(e.keyCode == 9 ){
+            console.log('tag');
+        }
+    });
+
+
